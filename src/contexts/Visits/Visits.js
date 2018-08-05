13ab66348fac3de1e7bd1d70c62e628046e7382a
Kristina Vragovic <k.vragovic@gmail.com>
2018-08-05 16:21:16 -0400

    Create visits list component

--- src/contexts/Visits/Visits.js
+++ src/contexts/Visits/Visits.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import AnimatedEllipsis from 'react-native-animated-ellipsis'
+
+import { generateVisits } from '../../utils/generateVisits'
+
+import VisitsList from './VisitsList'
+
 
 export class Visits extends Component {
-  render() {
+  state = {
+    loading: false,
+    visits: []
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true })
+
+    const visits = generateVisits()
+    this.setState({ visits, loading: false })
+  }
+ 
+
+  renderLoading = () => {
     return (
       <View>
-        <Text h1>Visits</Text>
+        <Text>
+          Loading Visits
+          <AnimatedEllipsis />
+        </Text>
       </View>
     )
+  }
+
+  render() {
+    if (this.state.loading) {
+      return this.renderLoading()
+
+    } else {
+      return (
+        <View>
+          <Text h1>Visits</Text>
+          <VisitsList visits={this.state.visits} />
+        </View>
+      )
+    }
   }
 }
 
---
+++ src/contexts/Visits/VisitsList.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text, View } from 'react-native'
+import { List, ListItem } from 'react-native-elements'
+
+export const VisitsList = (props) => {
+
+  doctorName = (doctor) => {
+    const { first_name, middle_name, last_name, title } = doctor.profile
+    return `${first_name} ${middle_name} ${last_name}, ${title}`
+  }
+
+  return (
+    <List>
+      {props.visits.map((visit, index) => (
+        <ListItem
+          key={index}
+          title={doctorName(visit.doctor)}
+          subtitle={
+            <View style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}>
+              <Text>{visit.date}</Text>
+              <Text>{visit.totalCost}</Text>
+            </View>
+          }
+        />
+      ))}
+    </List>
+  )
+}
+
+VisitsList.propTypes = {
+  visits: PropTypes.array.isRequired
+}
+
+export default VisitsList
