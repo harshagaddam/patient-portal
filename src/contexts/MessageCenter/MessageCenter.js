808677fc3fb00fd7a14c2fe40b292d68026a500c
Kristina Vragovic <k.vragovic@gmail.com>
2018-08-07 00:02:20 -0500

    Skeleton of message center

--- src/contexts/MessageCenter/MessageCenter.js
+++ src/contexts/MessageCenter/MessageCenter.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Text } from 'react-native-elements'
+import { Button } from 'react-native-elements'
+
+import { colors } from '../../utils/styleVariables'
+import MessageStyles from './MessageStyles'
 
 export class MessageCenter extends Component {
   render() {
     return (
-      <View>
-        <Text h1>Message Center</Text>
+      <View style={MessageStyles.container}>
+
+        <Button raised
+          title='Messages Received'
+          // onPress={() => { this.props.navigation.navigate('Received') }}
+          style={MessageStyles.button}
+          backgroundColor={colors.primary}
+        />
+
+        <Button raised
+          title='Messages Sent'
+          // onPress={() => { this.props.navigation.navigate('Sent') }}
+          style={MessageStyles.button}
+          backgroundColor={colors.primary}
+        />
+
+        <Button raised
+          title='Send a Message'
+          // onPress={() => { this.props.navigation.navigate('Compose') }}
+          style={MessageStyles.button}
+          backgroundColor={colors.primary}
+        />
       </View>
     )
   }
